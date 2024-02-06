7249b15161e4b8e15cb32f1b027e4046d15476d8
Subi <marc.subira@autonoma.cat>
2024-02-06 22:04:06 +0100

    new features

--- web_server/helper.py
+++ web_server/helper.py
@@ -1,3 +1,37 @@
+import logging
+
+class Logger:
+    def __init__(self, name, level=logging.DEBUG, log_format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
+        
+        formatter = logging.Formatter(log_format)
+
+        # Create console handler and set level to debug
+        ch = logging.StreamHandler()
+        ch.setLevel(level)
+        ch.setFormatter(formatter)
+
+        # Add console handler to logger
+        self.logger.addHandler(ch)
+        
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def warning(self, msg):
+        self.logger.warning(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+
+#Declare some constants
 PI = 3.14159265359
 PI2 = 2*PI
 
@@ -6,6 +40,9 @@ MAX_DISTANCE = 40
 MAX_NUMBER_GROUPS = 10
 MAX_NUMBER_POINTS = 20
 
+
+
+#Utility functions
 def validate_group_name(group_name: str):
     """
     Returns:
@@ -20,7 +57,6 @@ def validate_group_name(group_name: str):
         return group_name
     except:
         return False
-    
     
 def validate_angle(angle: float):
     """
@@ -53,7 +89,6 @@ def validate_distance(distance: float):
     except:
         return False
     
-
 def add_point(points: dict, group_name: str, angle: float, distance :float):
     """Add a point in the Points dictionary
     """
@@ -61,12 +96,12 @@ def add_point(points: dict, group_name: str, angle: float, distance :float):
         if len(points.keys()) < MAX_NUMBER_GROUPS:
             points[group_name] = {"points": [{"distance": distance, "angle": angle}
                                             ],
-                                  "number_points": 1
+                                  "points_number": 1
                                 }
     else:
         if points[group_name]["number_points"] < MAX_NUMBER_POINTS:
             points[group_name]["points"].append({"distance": distance, "angle": angle})
-            points[group_name]["number_points"] += 1
+            points[group_name]["points_number"] += 1
             
         else:
             points[group_name]["points"].pop(0)
--- web_server/serverHTTP.py
+++ web_server/serverHTTP.py
@@ -3,7 +3,6 @@ import json
 import helper
 import urllib.parse
 
-
 IP = "localhost"
 PORT = 8000
 
@@ -17,90 +16,146 @@ POINTS = {
             {"distance": 3, "angle": helper.PI2}, 
 
         ],
-        "number_points": 4
+        "points_number": 4
     }
 }
 
 
 # Definir un manejador de solicitudes personalizado que herede de BaseHTTPRequestHandler
-class RequestHandler(BaseHTTPRequestHandler):
+class RequestHandler(BaseHTTPRequestHandler):      
     
     def do_GET(self):
+        self.server.logger.debug(f"Recived GET request from {self.client_address[0]}")
+        
         parsed_url = urllib.parse.urlparse(self.path)
         query_params = urllib.parse.parse_qs(parsed_url.query)
 
-        if parsed_url.path == '/api/get_points':
-            group_name = query_params.get('group_name', [''])[0]
-            
-            if not group_name in POINTS.keys():
-                return
-                
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-
-            # Obtener la lista de cadenas y enviarla junto con la imagen
-            response_data = POINTS[group_name]
-            self.wfile.write(json.dumps(response_data).encode('utf-8'))
+        self.server.logger.debug(f"Recived url {parsed_url}")
+        
+        #Do not enable search for files out the directory
+        if ".." in parsed_url.path:
             return
         
+        elif parsed_url.path == '/api/get_points':
+            self._handle_api_get_points(query_params)
+            return
             
-        if self.path == '/':
-            # Si la solicitud GET es para '/', servir el archivo index.html
-            self.path = '/index.html'
-        try:
-            # Abre y lee el archivo solicitado
-            with open('./src/' + self.path, 'rb') as file:
-                content = file.read()
-            # Envía la respuesta con el contenido del archivo
-            self.send_response(200)
-            self.send_header("Content-type", "text/" + self.path.split(".")[-1])
-            self.end_headers()
-            self.wfile.write(content)
-        except FileNotFoundError:
-            # Si el archivo no se encuentra, enviar una respuesta 404
-            self.send_response(404)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write('404 Not Found'.encode("utf-8"))
+        elif parsed_url.path == '/':
+            # If the GET request is for '/', serve the file index.html
+            parsed_url = parsed_url._replace(path='/index.html')
+            
+        self._send_file(parsed_url.path)
 
     def do_POST(self):
+        self.server.logger.debug(f"Recived GET request from {self.client_address[0]}")
+        
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length).decode('utf-8')
+        path = post_data.get('path', '')
         
-        try:
-            data = json.loads(post_data)
-            print(data)
+        if path == "/api/send_point":
+            response_message = self._handle_api_send_point(post_data)
+        
+        else:
+            response_message = "Wrong POST Path. It should be /api/send_point."
             
-            group_name = helper.validate_group_name(data.get('name', ''))
-            angle = helper.validate_angle(data.get('angle', ''))
-            distance = helper.validate_distance(data.get('distance', ''))
-            
-            if group_name and angle and distance:            
-                response_message = f"Solicitud POST recibida con éxito. Group {group_name}, Angle {angle}, distance {distance}"
-                helper.add_point(POINTS, group_name, angle, distance)
-                print(response_message)
-            else:
-                response_message = "Wrong POST message."
-                
-        except json.JSONDecodeError:
-            response_message = "Error al decodificar JSON en la solicitud POST"
-
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(response_message.encode('utf-8'))
-        
-        
+            
+    def _send_file(self, path: str):
+        try:
+            # Open and read the requested file
+            with open('./src/' + path, 'rb') as file:
+                content = file.read()
 
-# Configurar el servidor con el manejador de solicitudes personalizado
-def run_server(ip = "", port=8000):
+            # Send the response with the file content
+            self.send_response(200)
+            self.send_header("Content-type", "text/" + path.split(".")[-1])
+            self.end_headers()
+            self.wfile.write(content)
+            
+        except FileNotFoundError:
+            # If the file is not in the sistem, send a 404 response
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write('404 Not Found'.encode("utf-8"))
+    
+    def _get_data_from_post_api(self, post_data: str):
+        """Returns group_name, angle and distance from the post_data
+        """
+        data = json.loads(post_data)
+        self.server.logger.debug(f"Data {data} recived")
+        
+        group_name = helper.validate_group_name(data.get('name', ''))
+        angle = helper.validate_angle(data.get('angle', ''))
+        distance = helper.validate_distance(data.get('distance', ''))
+        
+        return group_name, angle, distance
+
+    def _handle_api_get_points(self, query_params: dict):
+        """Function to handle the requests from the API getPoints
+        """
+        self.server.logger.debug("Get Points API request recived")
+        
+        group_name = query_params.get('group_name', [''])[0]
+            
+        if not group_name in POINTS.keys():
+            self.server.logger.error(f"{group_name} not in POINTS group name's")
+            return
+        
+        self.server.logger.debug(f"Group: {group_name}")
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        # Get the list of points and send them
+        response_data = POINTS[group_name]
+        self.wfile.write(json.dumps(response_data).encode('utf-8'))
+        
+        # Restart its points
+        #POINTS[group_name] = {"points": [], "points_number": 0}
+
+    def _handle_api_send_point(self, post_data: str):
+        """Function to handle the requests from the API sendPoints
+        """
+        self.server.logger.debug("Send Points API request recived")
+        
+        try:
+            group_name, angle, distance = self._get_data_from_post_api(post_data)
+            
+            if group_name and angle and distance:            
+                response_message = f"Solicitud POST recibida con éxito. Group {group_name}, Angle {angle}, distance {distance}"
+                helper.add_point(POINTS, group_name, angle, distance)
+                
+            else:
+                
+                response_message = "Wrong POST message. Group_name, angle or distance is not correct. Please check those values."
+                
+        except json.JSONDecodeError:
+            response_message = "Error al decodificar JSON en la solicitud POST"
+            
+        return response_message
+   
+class CustomHTTPServer(HTTPServer):
+    def __init__(self, server_address, RequestHandlerClass, logger):
+        self.logger = logger
+        super().__init__(server_address, RequestHandlerClass)
+   
+
+# Configure the server with the personalized request handler
+def run_server(ip = "localhost", port=8000):
     server_address = (ip, port)
-    httpd = HTTPServer(server_address, RequestHandler)
-    print(f"Servidor en ejecución en el puerto {port}")
+    logger = helper.Logger(__name__)
+    httpd = CustomHTTPServer(server_address, RequestHandler, logger)
+    print(f"Server executing on port {port} and address {ip}")
     httpd.serve_forever()
 
-# Ejecutar el servidor en el puerto 8000 (puedes cambiarlo si lo deseas)
-run_server(IP, PORT)
+
+# Execute the server with the IP and PORT specified
+if __name__ == "__main__":
+    run_server(IP, PORT)
 
 
